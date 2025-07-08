52b2f4a3063c76fe09eb284a55a0acadb98aa0f3
Aaditya <aadityakam@gmail.com>
2025-07-08 16:34:34 +0530

    Update ConfigManager.java

--- src/main/java/com/example/framework/config/ConfigManager.java
+++ src/main/java/com/example/framework/config/ConfigManager.java
@@ -1,1 +1,23 @@
+package com.example.framework.config;
 
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigManager {
+    private static final Properties props = new Properties();
+    static {
+        String env = System.getProperty("environment", "dev");
+        try (InputStream is = ConfigManager.class.getResourceAsStream("/env.properties")) {
+            props.load(is);
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot load env.properties", e);
+        }
+        prefix = env + ".";
+    }
+
+    private static String prefix;
+
+    public static String get(String key) {
+        return props.getProperty(prefix + key);
+    }
+}
